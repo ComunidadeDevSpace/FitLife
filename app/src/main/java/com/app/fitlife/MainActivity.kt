254cbc18735718c59601da127f48e8c020aae7e4
Bernardo Oechsler <bernardo.oechsler6@gmail.com>
2023-05-09 11:29:42 -0700

    Teste (Bernardo)

--- app/src/main/java/com/app/fitlife/MainActivity.kt
+++ app/src/main/java/com/app/fitlife/MainActivity.kt
@@ -3,6 +3,8 @@ package com.app.fitlife
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Teste
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
