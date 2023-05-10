04b6cc4b4c3145e14ede868417b415a9f7d7490a
Samuel Rocha Gil <samugil@gmail.com>
2023-05-10 14:49:42 +0100

    Tela principal

---
+++ app/src/main/java/com/app/fitlife/TelaPrincipalBotoesCalculo.kt
@@ -0,0 +1,11 @@
+package com.app.fitlife
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaPrincipalBotoesCalculo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_principal_botoes_calculo)
+    }
+}
